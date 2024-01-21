0de451fadd7095fcf692b79b99d42c23739caf2b
VANN YA <99462479+VannYa1212@users.noreply.github.com>
2024-01-21 21:08:06 +0700

    add

---
+++ assets/js/index.js
@@ -0,0 +1,31 @@
+ /* varaible var let const */
+ var a = 10; // global scope
+ let b = 20; // block scope
+ const c = 30; // block scope
+ console.log(a);
+ console.log(b);
+ console.log(c);
+ /* data type */
+let number1 = 269; 
+let number2 = 269.269;
+let rs = number2 / number1;
+let string = 'Hello World';
+let boolean = true; 
+let undefained = undefined;
+
+
+console.log(typeof number); // number
+console.log(typeof string); // string
+console.log(typeof boolean); // boolean
+console.log(typeof undefained); // undefained
+
+console.log(rs.toFixed(2)); // 1.00
+console.log(rs.toFixed(3)); // 1.000
+
+
+
+
+
+
+
+
